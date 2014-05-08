f43017c75dc38f9cdf8f5482434995f0534eeb8c
Raghuveer Guthikonda <raghuveer.g@teleparadigm.com>
2014-05-08 13:41:32 +0530

    contact us animation completed

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 
 $(function () {
+	//Application Menu Hide/Show based on user click
 	$("#app-nav-menu").on('click', 'a', function(){
 		$('.applications').fadeOut();
 		var $this = $(this);
@@ -15,7 +16,37 @@ $(function () {
 			$($this.attr('data-toggle')).fadeIn();
 			$this.addClass("active");	
 		}
-		
+	});
+
+	$('#contactus').delay(1000).animate({"right":-264},800,function(){
+			$(this).css({"transform":"rotate(15deg)"})
+	});
+	$('#contactus').bind({
+		mouseenter: function(e) {
+			$(this).css({"transform":"rotate(0deg)"}).stop(true).animate({"right":0},800);
+		},
+		mouseleave: function(e) {		
+			if($(this).hasClass('active') == false)
+			{
+				$(this).stop(true).animate({"right":-264},800,function(){
+					$(this).css({"transform":"rotate(15deg)"});
+				});	
+			}			
+		}
+	});
+	$(document).on('click', "#contactus", function (e) {
+		if(!($(this).hasClass('active')))
+		{
+			$(this).addClass('active');
+			$(this).css({"transform":"rotate(0deg)"}).stop(true).animate({"right":0},800);
+		}
+		else{
+			$(this).stop(true).animate({"right":-264},800,function(){
+				$(this).css({"transform":"rotate(15deg)"});
+			});	
+			$(this).removeClass('active');		
+		}
 		
 	});
+
 });
